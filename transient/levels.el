2028401d242ddc30848561e3666ad6326f079e42
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2017-11-22 16:57:21 +0100

    Continuously amended commit
    I stopped keeping history here, no point, I change it too often.
    Just one big continuously amended commit.

---
+++ transient/levels.el
@@ -0,0 +1,7 @@
+((magit-fetch
+  (transient:magit-fetch:--unshallow . 4))
+ (magit-gitignore
+  (magit-no-assume-unchanged . 4)
+  (magit-assume-unchanged . 4)
+  (magit-no-skip-worktree . 4)
+  (magit-skip-worktree . 4)))
